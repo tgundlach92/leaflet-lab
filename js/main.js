4b5022c4bd540957feb1cc87dc777495b9a2068d
tgundlach92 <46975649+tgundlach92@users.noreply.github.com>
2019-03-06 21:26:55 -0600

    Add files via upload
    Module 5B Assignment Completed

--- js/main.js
+++ js/main.js
@@ -13,70 +13,54 @@ function createMap(){
 
     getData(map);
 };
-//creates a popup that displays the properties for each feature displayed from the data
-function onEachFeature(feature, layer) {
-    var popupContent = "";
-    if (feature.properties) {
-        for (var property in feature.properties){
-            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
-        }
-        layer.bindPopup(popupContent);
-    };
-};
-// function getData(map){
-// //references jquery to grab the data
-//     $.ajax("data/RenewableEnergyProduction.geojson", {
-//         dataType: "json",
-//         success: function(response){
-// //defines the variable to stylize the markers of the data imported
-//             var geojsonMarkerOptions = {
-//                 radius: 8,
-//                 fillColor: "#c0c0c0",
-//                 color: "#000",
-//                 weight: 1,
-//                 opacity: 1,
-//                 fillOpacity: 0.8
-//             };
-// // defines each feature and applies the custom marker to each point
-// //           L.geoJson(response, {
-// //               onEachFeature: onEachFeature,
-// //               pointToLayer: function (onEachFeature, latlng){
-// //                   return L.circleMarker(latlng, geojsonMarkerOptions);
-// //               },
-// //           }).addTo(map);
-//         }
-//     });
-// };
-// function createPropSymbols(data, map){
-//
-//     var geojsonMarkerOptions = {
-//         radius: 8,
-//         fillcolor: "#003153",
-//         color: "#000",
-//         weight: 1,
-//         opacity: 1,
-//         fillOpacity: 0.8
-//     };
-//
-//     L.geoJson(data, {
-//
-//         pointToLayer: function (feature, latlng) {
-//             return L.circleMarker(latlng, geojsonMarkerOptions);
-//         }
-//     }).addTo(map);
-// };
-
-//function to create symbols and assigns the settings of the symbols
-function createPropSymbols(data, map){
-    var attribute = "2009";
-    var geojsonMarkerOptions = {
-        radius: 8,
-        fillcolor: "#003153",
+//function to convert markers to circle markers
+function pointToLayer(feature, latlng, attributes){
+    //Determine which attribute to visualize with proportional symbols
+    var attribute = attributes[0];
+    console.log(attribute);
+    //create marker options
+    var options = {
+        fillColor: "#003153",
         color: "#000",
         weight: 1,
         opacity: 1,
         fillOpacity: 0.8
     };
+
+    //For each feature, determine its value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
+    //Give each feature's circle marker a radius based on its attribute value
+    options.radius = calcPropRadius(attValue);
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, options);
+    //build popup content string
+    var popupContent = "<p><b>Country:</b> " + feature.properties.Country + "</p>";
+    var year = attribute.split("yr_")[1];
+    popupContent += "<p><b>Renewable Energy Production in " + year + ":</b> " + feature.properties[attribute] + " quadrillion BTU's</p>";
+    layer.bindPopup(popupContent, {
+        offset: new L.Point(0,-options.radius)
+    });
+    layer.on({
+        mouseover: function(){
+            this.openPopup();
+        },
+        mouseout: function(){
+            this.closePopup();
+        }
+    });
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+};
+
+//Add circle markers for point features to the map
+function createPropSymbols(data, map, attributes){
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng){
+            return pointToLayer(feature, latlng, attributes);
+        }
+    }).addTo(map);
+};
 //function to calculate the radius of the circles based on the attribute value
 function calcPropRadius(attValue) {
     var scaleFactor = 50;
@@ -85,22 +69,79 @@ function calcPropRadius(attValue) {
 
     return radius;
 };
-//creates the layer with the returned proportional symbols and settings
-    L.geoJson(data, {
-        pointToLayer: function (feature, latlng) {
-            var attValue = Number(feature.properties[attribute]);
-            console.log(feature.properties, attValue);
-            geojsonMarkerOptions.radius = calcPropRadius(attValue);
-            return L.circleMarker(latlng, geojsonMarkerOptions);
-        }
-    }).addTo(map);
+function createSequenceControls(map, attributes){
+    $('#panel').append('<input class="range-slider" type="range">');
+    $('#panel').append('<button class="skip" id="reverse">Back</button>');
+    $('#panel').append('<button class="skip" id="forward">Skip</button>');
+    $('#reverse').html('<img src="img/Reverse.png">');
+    $('#forward').html('<img src="img/Forward.png">');
+
+    $('.range-slider').attr({
+        max: 6,
+        min: 0,
+        value: 0,
+        step: 1
+    });
+
+    //Step 5: click listener for buttons
+    $('.skip').click(function(){
+        //get the old index value
+        var index = $('.range-slider').val();
+        //Step 6: increment or decrement depending on button clicked
+        if ($(this).attr('id') == 'forward'){
+            index++;
+            //Step 7: if past the last attribute, wrap around to first attribute
+            index = index > 6 ? 0 : index;
+        } else if ($(this).attr('id') == 'reverse'){
+            index--;
+            //Step 7: if past the first attribute, wrap around to last attribute
+            index = index < 0 ? 6 : index;
+        };
+        //update slider
+        $('.range-slider').val(index);
+        //input listener for slider
+        $('.range-slider').on('input', function(){
+            //sequence
+        });
+        //pass new attribute to update symbols
+        updatePropSymbols(map, attributes[index]);
+    });
 };
-//grabs the data via ajax from the gejson file
+function updatePropSymbols(map, attribute){
+    map.eachLayer(function(layer){
+        if (layer.feature && layer.feature.properties[attribute]){
+            var props = layer.feature.properties;
+            var radius = calcPropRadius(props[attribute]);
+            layer.setRadius(radius);
+            var popupContent = "<p><b>Country:</b> " + props.Country + "</p>";
+            var year = attribute.split("yr_")[1];
+            popupContent += "<p><b>Renewable Energy Production in " + year + ":</b> " + props[attribute] + " quadrillion BTU's</p>";
+            layer.bindPopup(popupContent, {
+                offset: new L.Point(0,-radius)
+            });
+        };
+    });
+};
+function processData(data){
+    var attributes = [];
+    var properties = data.features[0].properties;
+
+    for (var attribute in properties){
+        if (attribute.indexOf("yr_") > -1){
+            attributes.push(attribute);
+        };
+    };
+    console.log(attributes);
+    return attributes;
+};
 function getData(map){
     $.ajax("data/RenewableEnergyProduction.geojson", {
         dataType: "json",
         success: function(response){
-            createPropSymbols(response, map);
+
+            var attributes = processData(response);
+            createPropSymbols(response, map, attributes);
+            createSequenceControls(map, attributes);
         }
     });
 };
